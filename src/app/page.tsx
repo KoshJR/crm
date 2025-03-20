8c215612eab54a0927a0653bd90e37310f6db1e1
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2025-03-20 18:23:24 +0100

    Final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-import { headers } from 'next/headers';
-import StatusLabel, { Status } from './components/status-label';
-import AddCompanyButton from './components/add-company-button';
+import Link from "next/link";
 
 export default function Home() {
-  console.log(headers());
-  
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <AddCompanyButton/>
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 p-6">
+        <div className="bg-white shadow-lg rounded-2xl p-8 text-center max-w-md">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4">Customer Relationship Management</h1>
+          <Link
+            href={'/companies'}
+            className="text-white bg-gray-900 hover:bg-blue-700 font-medium py-2 px-4 rounded-lg transition"
+          >
+            Continue to Dashboard
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
