c548d059c38beb042099565ae6e4b85bd3233bd8
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2025-03-20 18:45:39 +0100

    Props fix

--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -1,16 +1,17 @@
-'use client';
-
 import React from 'react';
 import PromotionForm from '@/app/components/promotion-form';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>; // Змінили на Promise
 }
 
-export default function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  // Дескрипція params для асинхронної обробки
+  const { id } = await params;
+
   return (
     <div className="py-6 px-10">
-      <PromotionForm companyId={params.id} />
+      <PromotionForm companyId={id} />
     </div>
   );
 }
